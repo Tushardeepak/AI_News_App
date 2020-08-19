8db0772418a135257008b97faa520e72050810a3
Tushar Deepak <39951979+Tushardeepak@users.noreply.github.com>
2020-08-19 23:22:57 +0530

    Updates

---
+++ precache-manifest.d72ef9b3e1efe7f39514e822545f17c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d130ef2ba6b587fef74fe3e6470752",
+    "url": "/AI_News_App/index.html"
+  },
+  {
+    "revision": "de7f43c15e3d039cbe33",
+    "url": "/AI_News_App/static/css/main.8c927a5a.chunk.css"
+  },
+  {
+    "revision": "9ada551020d1ddc88865",
+    "url": "/AI_News_App/static/js/2.f2c91d73.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/AI_News_App/static/js/2.f2c91d73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de7f43c15e3d039cbe33",
+    "url": "/AI_News_App/static/js/main.14a3f561.chunk.js"
+  },
+  {
+    "revision": "555165fdee82d5bfc4e5",
+    "url": "/AI_News_App/static/js/runtime-main.fe2fc642.js"
+  }
+]);
